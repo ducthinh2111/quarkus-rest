aaaf47055c75dde9cfcfa9e28543e71fa1ad51d9
Thinh Le <thinhle@Thinhs-MacBook-Pro.local>
2021-09-30 23:49:50 +0700

    initial commit

---
+++ src/native-test/java/com/starea/NativeTodoResourceIT.java
@@ -0,0 +1,9 @@
+package com.starea;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeTodoResourceIT extends TodoResourceTest {
+
+    // Execute the same tests but in native mode.
+}
